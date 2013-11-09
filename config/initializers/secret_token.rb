729181ab4c9c74d6ed8d8bbb5ae69156c5821c7b
Masato Ikeda <masato.ikeda@gmail.com>
2013-11-09 23:43:56 +0900

    rails new sorcery_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SorceryApp::Application.config.secret_key_base = '3b6a8b9a3df009538dc15e01c2b69b5dad4ee4454c427a244fecf6f783db944b5176ffeb7184493879bcddc84544e1ff7ac11c6109702aba2b89262e9e75b2a0'
