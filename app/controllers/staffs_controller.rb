50823bda98c11525658a55136cf2313f97ed3d94
Masato Ikeda <masato.ikeda@gmail.com>
2013-11-10 00:32:19 +0900

    sign up form

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,4 +1,20 @@
 class StaffsController < ApplicationController
   def new
+    @staff = Staff.new
+  end
+
+  def create
+    @staff = Staff.new(staff_params)
+    @staff.save!
+    redirect_to root_url, notice: "Signed up!"
+
+  rescue
+    render :new
+  end
+
+  private
+
+  def staff_params
+    params.require(:staff).permit(:username, :email, :password, :password_confirmation)
   end
 end
